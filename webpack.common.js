26f8299b0c3f89a2ed90fb4a42a8180031949819
Kevin Bai <kevin.c.bai0@gmail.com>
2019-12-27 01:47:10 -0500

    Updated to working npm package

--- webpack.common.js
+++ webpack.common.js
@@ -2,13 +2,16 @@ const path = require("path");
 
 module.exports = {
     entry: "./src/index",
+    target: "web",
     resolve: {
         extensions: ['.ts', '.tsx', '.js', '.json']
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'app.bundle.js',
-        library: "ArtGen"
+        library: "ArtGen",
+        libraryTarget: 'umd',
+        globalObject: 'this',
     },
     module: {
         rules: [
