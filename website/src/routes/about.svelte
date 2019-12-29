11f9594401701b3e00de247998f8ef52b443c9d3
Kevin Bai <kevin.c.bai0@gmail.com>
2019-12-29 18:52:51 -0500

    created website dir

---
+++ website/src/routes/about.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<style>
+	p {
+		margin: 0
+	}
+</style>
+
+<h1>About this site</h1>
+
+<p>This is the 'about' page. There's not much here.</p>
---
+++ website/src/routes/index.svelte
@@ -0,0 +1,12 @@
+
+<svelte:head>
+	<title>Sapper project template</title>
+</svelte:head>
+
+<style>
+	p {
+		margin: 0;
+	}
+</style>
+
+<p>Hello</p>
